5a68000da09f57a2bb82a302b40281fc904a8ecf
mnhat05 <147588250+mnhat05@users.noreply.github.com>
2023-10-11 15:34:34 +0700

    Add files via upload

---
+++ bài TH1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+ { 
+ int n, a[10];
+   printf("\n phan tu cua mang  n=");
+   scanf("%d",&n);
+   for (int i=0; i<n; i++)
+   { 
+   printf("nhap a[%d]=",i);
+  scanf("%d",&a[i]);
+   }
+   return 0;
+}
+
